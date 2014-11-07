5817801c26cfc46231b65896d6dfb396a4c5bfd3
ViridianForge <viridianforge@gmail.com>
2014-11-06 16:37:37 -0800

    Exceptions and more URLS
    Filling up the URL database through October, and adding exception
    checking to the code to allow for processing URLs that don't process
    well.

--- bcScrape.py
+++ bcScrape.py
@@ -15,33 +15,39 @@ with open ('./bcURLList.txt','r') as urlFile:
 		lineNum += 1
 		#Get the raw HTML of the album's website
 		albumURL = line.rstrip()
-		print albumURL
-		page = requests.get(albumURL)
-		tree = html.fromstring(page.text)
+		print "Querying " + albumURL
+		try:
+			page = requests.get(albumURL)
+			tree = html.fromstring(page.text)
 
-		#Grab the meta elements that describe the album and a link to its 
-		#album art thumbnail
-		descElem = tree.xpath('//meta[@name="Description"]')[0]
-		albumArtElem = tree.xpath('//link[@rel="shortcut icon"]')[0]
+			#Grab the meta elements that describe the album and a link to its 
+			#album art thumbnail
+			descElem = tree.xpath('//meta[@name="Description"]')[0]
+			albumArtElem = tree.xpath('//link[@rel="shortcut icon"]')[0]
 
-		#Isolate the content of the description and the href of the album art
-		fullDesc = descElem.get("content").splitlines()[1]
-		albumArtLink = albumArtElem.get("href")
+			#Isolate the content of the description and the href of the album art
+			fullDesc = descElem.get("content").splitlines()[1]
+			#A thought here would be to download the image, and save it to a images folder in the Chipchart.
+			albumArtLink = albumArtElem.get("href")
 
-		#Split up the full description into title, artist, and release date
-		descPortions = fullDesc.split(",")
-		title =  descPortions[0].split(" by ")[0]
-		artist = descPortions[0].split(" by ")[1]
-		releaseDate =  descPortions[1].split("released ")[1]
+			#Split up the full description into title, artist, and release date
+			descPortions = fullDesc.split(",")
+			title =  descPortions[0].split(" by ")[0]
+			artist = descPortions[0].split(" by ")[1]
+			releaseDate =  descPortions[1].split("released ")[1]
 
-		#Finally, build the addition for the ChipChart Array Element
-		albumArtOut = "<a href='" + albumURL + "'><img title='" + title + "' src='" + albumArtLink + "'></a>"
+			#Finally, build the addition for the ChipChart Array Element
+			albumArtOut = "<a href='" + albumURL + "'><img title='" + title + "' src='" + albumArtLink + "'></a>"
 		
-		entry.append(albumArtOut)
-		entry.append(artist)
-		entry.append(releaseDate)
-		outData.append(entry)
-
+			entry.append(albumArtOut)
+			entry.append(artist)
+			entry.append(releaseDate)
+			outData.append(entry)
+		except:
+			print "Some invalid data at:"
+			print albumURL
+			print "Continuing processing."
+			
 #Put together the final dictionary to play nicely with the formatting DataTables is expecting
 outJSON = dict();
 outJSON["chartData"] = outData
@@ -49,4 +55,3 @@ outJSON["chartData"] = outData
 #Save and overwrite the previous .js array object
 with open('./chartList.json','w') as outFile:
 	json.dump(outJSON, outFile)
-
